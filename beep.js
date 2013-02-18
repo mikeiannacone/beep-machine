bab07b870d39dc8afa0aebf739bb7f17eb5ff30d
euf <euf@ornl.gov>
2013-02-17 20:49:52 -0500

    option to add more measures

--- beep.js
+++ beep.js
@@ -6,6 +6,7 @@ var notes = [48, 50, 52, 53, 55, 57, 59, 60]
 var current_col = 0;
 var rows = 8
 var cols = 8
+var maxCols = 8 * 12
 
 var bpm = 120*2 //each sq is 1/2 beat
 
@@ -69,6 +70,30 @@ function unHilightCol(col){
   }
 }
 
+//will show/hide cols as needed so that 'n' measures are visible
+function showMeasures(n){
+  for(var j=0; j<(maxCols/8); j++){
+    if(j < n)
+      $('.space_' + j).show()
+    else
+      $('.space_' + j).hide()
+  }
+  cols = 8*n;
+  //console.log("showMeasures: " + n)
+  for(var j=0; j<maxCols; j++){
+    for(var i=0; i<rows; i++){
+      if(j < cols)
+        $('#cell_' + i + '_' + j).show()
+      else{
+        //$('#cell_' + i + '_' + j).removeClass('current')
+        $('#cell_' + i + '_' + j).hide()
+      }
+    }
+  }
+  $('td.current').removeClass('current')
+  hilightCol(current_col)
+}
+
 $().ready(function () {
   
   MIDI.loadPlugin({
@@ -82,18 +107,24 @@ $().ready(function () {
 
   for(var i=(rows-1); i>=0; i--){//want 0,0 to be the bottom left.
     $('#buttongrid_1').append('<tr id="row_' + i + '">');
-    for(var j=0; j<cols; j++){ 
+    for(var j=0; j<maxCols; j++){ 
+      if(j !== 0 && j%8 === 0) //add space cell every measure
+        $('#row_' + i).append('<td class="space space_' + (j/8) + '"> </td>');
       $('#row_' + i).append('<td id="cell_' + i + '_' + j + '" width="20" height="20"> </td>');
       $('#cell_' + i + '_' + j).bind('click', function(event) {
-
         var tgt = $(event.target);
         if(tgt.hasClass('selected')) tgt.removeClass('selected');
         else tgt.addClass('selected');
-
         event.preventDefault();
       });
     }
   }
+  showMeasures(1);
+
+  $('#controlsForm').bind('submit', function(event) {
+    event.preventDefault();
+    return false;
+  });
 
   $('#goButton').bind('click', function(event) {
     if(!loopInterval)
@@ -113,9 +144,16 @@ $().ready(function () {
 
   $('#bpmSlider').bind('change', function(event) {
     bpm = 2 * $('#bpmSlider').val();
-    clearInterval(loopInterval)
-    loopInterval = setInterval(function(){handleCol()},1000*60/bpm);
+    if(loopInterval){
+      clearInterval(loopInterval);
+      loopInterval = setInterval(function(){handleCol()},1000*60/bpm);
+    }
     $('#currBPM').text(bpm/2);
+  });
+
+  $('#numMeasures').bind('change', function(event) {
+    var measures = $('#numMeasures').val();
+    showMeasures(measures);
   });
   
 });
